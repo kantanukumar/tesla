80189a9cfce4e4c192c2c3b04b02cdd88a483ae2
Kantanu Mohapatra <kantanukumar@gmail.com>
2023-07-17 11:56:01 +0530

    Hello World from CLI

---
+++ daily/HelloWorld.c
@@ -0,0 +1,16 @@
+/*
+ *
+ * Though this is a C file, but I am adding this just to experiment my token
+ *
+ */
+
+#include <stdio.h>
+
+int main()
+{
+  printf("Hello, World\n");
+
+  printf("Welcome to the github from CLI\n");
+
+  return 0;
+}
