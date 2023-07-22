1e632ff1d25b47ebb9a4dc9f146296aba3318e8a
Kantanu Mohapatra <kantanukumar@gmail.com>
2023-07-22 20:35:12 +0530

    - Update to tree utilities.

--- daily/d10_tree_create.c
+++ daily/d10_tree_create.c
@@ -3,6 +3,9 @@
  * Date: 16/07/23
  * Problem: Create a tree.
  * Approach: Use left and right pointers, which will point to child nodes.
+ *            We can pass different parameters to createTreeNode() with values
+ *            upto level 3 only. A better approach will be using Queue,
+ *            which I will update in another file.
  * Files: d10_tree_create.c
  */
 
@@ -13,195 +16,8 @@ struct tNode
 {
   struct tNode *left;
   int data;
-  struct tNode *left;
-};
-
-void insert_begin(int data)
-{
-  printf("Insert %d at start of DLL\n", data);
-
-  struct node *newNode = (struct node*)malloc(sizeof(struct node));
-  newNode->prev = NULL;
-  newNode->next = NULL;
-  newNode->data = data;
-
-  if(head == NULL)
-  {
-    head = newNode;
-  }
-  else
-  {
-     newNode->next = head;
-     newNode->prev = head->prev;
-     head = newNode;
-  }
-}
-
-void insert_end(int data)
-{
-  printf("Insert %d at end of DLL\n", data);
-
-  struct node *newNode, *temp = head;
-
-  newNode = (struct node*)malloc(sizeof(struct node));
-  newNode->prev = NULL;
-  newNode->next = NULL;
-  newNode->data = data;
-
-  if(head == NULL)
-  {
-    head = newNode;
-  }
-  else
-  {
-    // Traverse till NULL
-    while(temp->next)
-    {
-      temp = temp->next;
-    }
-    temp->next = newNode;
-    newNode->prev = temp;
-  }
-}
-
-void display()
-{
-  struct node *temp = head;
-
-  printf("The DLL is: NULL<->");
-
-  while(temp)
-  {
-    printf("%d<->", temp->data);
-    temp = temp->next;
-  }
-
-  printf("NULL\n");
-}
-
-int length()
-{
-  struct node *temp = head;
-  int count = 0;
-
-  printf("The DLL is: NULL<->");
-
-  while(temp)
-  {
-    count++;
-    printf("%d<->", temp->data);
-    temp = temp->next;
-  }
-
-  printf("NULL\n");
-  printf("Len=%d\n", count);
-
-  return count;
-}
-
-void insert_pos(int pos, int data)
-{
-  printf("Inserting %d at pos %d\n", data, pos);
-
-  if(pos > length())
-  {
-    printf("Inserting pos beyond len\n");
-    return;
-  }
-  else
-  {
-    if(head == NULL && pos != 1)
-    {
-      printf("No nodes in DLL\n");
-      return;
-    }
-    else
-    {
-      struct node *newNode, *temp = head;
-
-      newNode = (struct node*)malloc(sizeof(struct node));
-      newNode->prev = NULL;
-      newNode->next = NULL;
-      newNode->data = data;
-
-      if(pos == 1)
-      {
-        // First node
-        head->next->prev = newNode;
-        newNode->next = head;
-        head = newNode;
-        return;
-      }
-      else
-      {
-        int idx = 1;
-        while(idx < (pos-1))
-        {
-          temp = temp->next;
-          ++idx;
-        } 
-
-        if(temp->next)
-        {
-          temp->next->prev = newNode;
-          newNode->next = temp->next;
-        }
-        newNode->prev = temp;
-        temp->next = newNode;
-      }
-    } 
-  }
-}
-
-void delete_pos(int pos)
-{
-  printf("Deleting pos %d\n", pos);
-
-  if(pos > length())
-  {
-    printf("Error: Deleting pos beyond len\n");
-    return;
-  }
-  else
-  {
-    if(head == NULL && pos != 1)
-    {
-      printf("Error: No nodes in DLL\n");
-      return;
-    }
-    else
-    {
-      struct node *temp = head;
-
-      if(pos == 1)
-      {
-        // Delete the first node
-        head = temp->next;
-        
-        // Checks if we have second node or not
-        if(head)  
-          head->prev = NULL;
-
-        free(temp);
-        return;
-      }
-      else // Any node, doesn't matter
-      {
-        int idx = 1;
-        while(idx++ < pos-1)
-          temp = temp->next;
-
-        struct node* delNode = temp->next;
-        temp->next = delNode->next;
-
-        if(delNode->next)
-          delNode->next->prev = temp;
-
-        free(delNode);
-      }
-    }
-  }
-}
+  struct tNode *right;
+}*rootTree = NULL;
 
 struct tNode* createTreeNode(int data)
 {
@@ -214,42 +30,93 @@ struct tNode* createTreeNode(int data)
   return newNode;
 }
 
+// Preorder traversal
+// Root->left->right 1-2-4-5-3-6-7
+void preOrder(struct tNode *root)
+{
+  // base condition, end the recursion
+  if(root == NULL)
+    return;
+
+  printf("%d ", root->data);
+  preOrder(root->left);
+  preOrder(root->right);
+}
+
+// Postorder traversal
+// left->right->Root 4-5-2-6-7-3-1
+void postOrder(struct tNode *root)
+{
+  // base condition, end the recursion
+  if(root == NULL)
+    return;
+
+  postOrder(root->left);
+  postOrder(root->right);
+  printf("%d ", root->data);
+}
+
+int isMirror(struct tNode *root1, struct tNode *root2)
+{
+  // Conditions to be symmetric,
+  // 1. When both the nodes are empty at the same time
+  // 2. When left node of root1 == right node of root2 &&
+  // 3. When left subtree of root == right subtree
+
+  // Cond - 1, An empty tree is symmetric
+  if(root1 == NULL && root2 == NULL)
+    return 1;
+
+  // Cond 2 && 3, Subtree match
+  if((root1 && root2) && (root1->data == root2->data))
+      return isMirror(root1->left, root2->right) &&
+                isMirror(root1->right, root2->left);
+  else
+    return 0;
+}
+
+// This routine has to call the isMirror() recursively
+int isSymmetry(struct tNode *root)
+{
+  return isMirror(root, root);
+}
+
+struct tNode* createTree()
+{
+  struct tNode *root = createTreeNode(1);
+
+  root->left = createTreeNode(2);
+  root->right= createTreeNode(2);
+#if 1
+  root->left->left = createTreeNode(4);
+// root->left->right = createTreeNode(5);
+
+//  root->right->left = createTreeNode(5);
+  root->right->right= createTreeNode(4);
+#endif
+  return root;
+}
+
 int main(int argc, char *argv[])
 {
   // Function body
-  if(argc > 1)
-  {
-    printf("Passed more than one argument, process it\n\n");
 
-    // Process the arguments, those contains the node value
+  /* First just create a Binary Tree which looks like below.
+    *           1
+    *           /\
+    *          2  3
+    *         /\  /\
+    *        4  5 6 7
+    */
+  rootTree = createTree();
 
-    int idx = 1;
-    int data = 0;
+  printf("\nPreorder of nodes 1-2-3-4-5-6-7: ");
+  preOrder(rootTree);
 
-    struct tNode *root = createTreeNode(1);
+  printf("\nPostorder of nodes 1-2-3-4-5-6-7: ");
+  postOrder(rootTree);
 
-    root->left = createTreeNode(2);
-    root->right= createTreeNode(3);
-
-
-    while(i != 15)
-    {
-      temp = createTreeNode():
-      temp->right= createTreeNode();
-      temp->left = createTreeNode();
-
-
-    }
-
-    while(argc != 1)
-    {   
-      data = atoi(argv[idx]);
-      createTreeNode(data);
-      ++idx; --argc;
-    }
-  }
-
-  display();
+  printf("\nBT %s symmetrical\n", isSymmetry(rootTree) ? "is" : "is not");
 
   printf("\nEnd of the program\n");
 
